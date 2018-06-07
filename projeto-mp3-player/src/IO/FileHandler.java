4844da5b444ba8ee113782312ad586870b8d1fcd
isaken <isaken@A309-02.imd.local>
2018-06-07 12:17:52 -0300

    Commit inicial.

---
+++ projeto-mp3-player/src/IO/FileHandler.java
@@ -0,0 +1,13 @@
+package IO;
+
+import java.io.File;
+/**
+ *
+ * @author Isaac Kennedy, Ian Honorato
+ * @version 1.0
+ * Esta classe tem como função ler e/ou inicializar os arquivos e pastas 
+ * necessárias a aplicação.
+ */
+public class FileHandler {
+    
+}
